f99118877dbcb76ef7983a6cd6a04629db132d0b
wayne <bahodirrisqulov176@gmail.com>
2022-05-07 15:08:48 +0500

    rwe
    wre

--- valmari/script.js
+++
@@ -1,8 +0,0 @@
-const burger = document.querySelector(".header__burger");
-const menu = document.querySelector(".header__menu");
-
-burger.addEventListener("click",
-	function () {
-		burger.classList.toggle("active");
-		menu.classList.toggle("active")
-	});
